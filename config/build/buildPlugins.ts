73936eb2ce8cf182bc9894177daa0390594deb53
YuriyYurev <iuriev-yuriy@mail.ru>
2025-03-15 13:46:19 +0700

    fix: decomposition

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import { WebpackPluginInstance, ProgressPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
+  return [
+    new ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+  ];
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import { Configuration } from "webpack";
+import { BuildOptions, BuildPaths } from "./types/config";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolves } from "./buildResolves";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths } = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolves(),
+  };
+}
