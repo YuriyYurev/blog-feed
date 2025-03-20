bc3f2738f2146c20383751e77b13ea9fb2145223
YuriyYurev <iuriev-yuriy@mail.ru>
2025-03-20 09:35:26 +0700

    feat: react-router-dom && Code splitting React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Counter from "./components/Counter";
-import './index.scss'
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import "./index.scss";
+
 export default function App() {
   return (
     <div className="app">
-        <h1>Counter</h1>
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О компании</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import Counter from "../../components/Counter";
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  );
+}
