7a13ab019b0702436aa435e02e68b6b908f7d034
YuriyYurev <iuriev-yuriy@mail.ru>
2025-06-20 09:47:28 +0700

    feat: sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { classNames } from "shared/lib/classNames/classNames";
+import { Sidebar } from "widgets/Sidebar";
 
 export default function App() {
   const { theme } = useTheme();
@@ -10,8 +11,10 @@ export default function App() {
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <AppRouter />
-      
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 }
