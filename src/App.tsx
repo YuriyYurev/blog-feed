cd8fbe54e7e1f27916f0a7cd4de22eba55dda706
YuriyYurev <iuriev-yuriy@mail.ru>
2025-10-20 20:49:11 +0700

    1.6 react-router-dom & code-splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/HomePage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
+
 export const App = () => {
   return (
     <div className="app">
-      <h1>Hello React</h1>
+      <Link to={"/"}>MainPage</Link>
+      <Link to={"/about"}>AboutPage</Link>
+      <Suspense fallback={<h1>Загрука</h1>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
