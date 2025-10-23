055e380c89a80e4d54e930939fd151ee5142c94d
YuriyYurev <iuriev-yuriy@mail.ru>
2025-10-23 23:05:36 +0700

    15 i18n LangSwitcher

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next";
+
 const AboutPage = () => {
+  const { t } = useTranslation("about");
   return (
     <>
-      <h1>About Page</h1>
+      <h1>{t("О сайте")}</h1>
     </>
   );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next";
+
 const MainPage = () => {
+  const { t } = useTranslation();
   return (
     <>
-      <h1>MainPage</h1>
+      <h1>{t("Главная страница")}</h1>
     </>
   );
 };
